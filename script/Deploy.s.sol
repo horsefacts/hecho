e880883b511dcca0a9b5cef8b60df98cb7cf65b6
horsefacts <webmaster@terminally.online>
2023-02-08 22:11:40 -0500

    Update deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,13 +4,12 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Script.sol";
 
-interface SimpleStore {
-    function setValue(uint256) external;
-    function getValue() external returns (uint256);
+interface IEcho {
+    function echo(string calldata) external;
 }
 
 contract Deploy is Script {
-    function run() public returns (SimpleStore simpleStore) {
-        simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
+    function run() public returns (IEcho echo) {
+        echo = IEcho(HuffDeployer.deploy("Echo"));
     }
 }
